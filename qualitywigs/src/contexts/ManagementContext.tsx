dbb24cbdbcf0eec31033fdb4b274fe98ce01c58b
kavetech2023 <kavetech@gmail.com>
2024-12-19 17:06:37 +0300

    remove unused variables

--- qualitywigs/src/contexts/ManagementContext.tsx
+++ qualitywigs/src/contexts/ManagementContext.tsx
@@ -4,6 +4,7 @@ import React, { createContext, useContext, useState, useEffect } from 'react'
 import { db } from '../lib/firebase'
 import { collection, addDoc, getDocs, deleteDoc, doc, updateDoc } from 'firebase/firestore'
 
+
 type Employee = {
   id: string
   name: string
